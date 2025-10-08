181840a7055256bea5399c5b0df81bc8fa0810fe
Agbajestephen <agbajestephen5@gmail.com>
2025-10-08 01:17:03 +0100

    update js

--- app.js
+++ app.js
@@ -6,44 +6,19 @@ const mongoose = require("mongoose");
 
 require("dotenv/config");
 const api = process.env.API_URL;
+const productRouter=require('./routers/products')
 
 //middlewaree
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
-const productsSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: Number,
-});
+app.use(`${api}/products`, productRouter)
 
-const Product = mongoose.model('Product',productsSchema)
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "hair dresser",
-    Image: "some_url",
-  };
-  res.send(product);
-});
+const Product = require('./models/product');
 
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name:req.body.name,
-        image:req.body.image,
-        countInStock:req.body.countInStock
-    })
 
-    product.save().then((createdProduct =>{
-        res.status(201).json(createdProduct)
-    }))
-  res.send(newProduct);
-});
-
-mongoose
-  .connect(process.env.CONNECTION_STRING, {
-    //d
+mongoose.connect(process.env.CONNECTION_STRING, {
     // useNewUrlParser : true,
     // useUnifiedTopology : true,
     dbName: "eshop-database",
